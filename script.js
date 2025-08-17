dc07b827821c05f2e2eea5096bf41b9911354e7a
yarikyoucan <yaroslav.andrunyk@gmail.com>
2025-08-17 21:11:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 "use strict";
 console.clear();
+
 class Stage {
     constructor() {
         // container
@@ -54,9 +55,9 @@ class Stage {
         this.camera.updateProjectionMatrix();
     }
 }
+
 class Block {
     constructor(block) {
-        // set size and position
         this.STATES = { ACTIVE: 'active', STOPPED: 'stopped', MISSED: 'missed' };
         this.MOVE_AMOUNT = 12;
         this.dimension = { width: 0, height: 0, depth: 0 };
@@ -65,7 +66,6 @@ class Block {
         this.index = (this.targetBlock ? this.targetBlock.index : 0) + 1;
         this.workingPlane = this.index % 2 ? 'x' : 'z';
         this.workingDimension = this.index % 2 ? 'width' : 'depth';
-        // set the dimensions from the target block, or defaults.
         this.dimension.width = this.targetBlock ? this.targetBlock.dimension.width : 10;
         this.dimension.height = this.targetBlock ? this.targetBlock.dimension.height : 2;
         this.dimension.depth = this.targetBlock ? this.targetBlock.dimension.depth : 10;
@@ -73,30 +73,24 @@ class Block {
         this.position.y = this.dimension.height * this.index;
         this.position.z = this.targetBlock ? this.targetBlock.position.z : 0;
         this.colorOffset = this.targetBlock ? this.targetBlock.colorOffset : Math.round(Math.random() * 100);
-        // set color
         if (!this.targetBlock) {
             this.color = 0x333344;
-        }
-        else {
+        } else {
             let offset = this.index + this.colorOffset;
             var r = Math.sin(0.3 * offset) * 55 + 200;
             var g = Math.sin(0.3 * offset + 2) * 55 + 200;
             var b = Math.sin(0.3 * offset + 4) * 55 + 200;
             this.color = new THREE.Color(r / 255, g / 255, b / 255);
         }
-        // state
         this.state = this.index > 1 ? this.STATES.ACTIVE : this.STATES.STOPPED;
-        // set direction
         this.speed = -0.1 - (this.index * 0.005);
-        if (this.speed < -4)
-            this.speed = -4;
+        if (this.speed < -4) this.speed = -4;
         this.direction = this.speed;
-        // create block
         let geometry = new THREE.BoxGeometry(this.dimension.width, this.dimension.height, this.dimension.depth);
         geometry.applyMatrix(new THREE.Matrix4().makeTranslation(this.dimension.width / 2, this.dimension.height / 2, this.dimension.depth / 2));
         this.material = new THREE.MeshToonMaterial({ color: this.color, shading: THREE.FlatShading });
         this.mesh = new THREE.Mesh(geometry, this.material);
-        this.mesh.position.set(this.position.x, this.position.y + (this.state == this.STATES.ACTIVE ? 0 : 0), this.position.z);
+        this.mesh.position.set(this.position.x, this.position.y, this.position.z);
         if (this.state == this.STATES.ACTIVE) {
             this.position[this.workingPlane] = Math.random() > 0.5 ? -this.MOVE_AMOUNT : this.MOVE_AMOUNT;
         }
@@ -107,10 +101,7 @@ class Block {
     place() {
         this.state = this.STATES.STOPPED;
         let overlap = this.targetBlock.dimension[this.workingDimension] - Math.abs(this.position[this.workingPlane] - this.targetBlock.position[this.workingPlane]);
-        let blocksToReturn = {
-            plane: this.workingPlane,
-            direction: this.direction
-        };
+        let blocksToReturn = { plane: this.workingPlane, direction: this.direction };
         if (this.dimension[this.workingDimension] - overlap < 0.3) {
             overlap = this.dimension[this.workingDimension];
             blocksToReturn.bonus = true;
@@ -129,24 +120,17 @@ class Block {
             let choppedGeometry = new THREE.BoxGeometry(choppedDimensions.width, choppedDimensions.height, choppedDimensions.depth);
             choppedGeometry.applyMatrix(new THREE.Matrix4().makeTranslation(choppedDimensions.width / 2, choppedDimensions.height / 2, choppedDimensions.depth / 2));
             let choppedMesh = new THREE.Mesh(choppedGeometry, this.material);
-            let choppedPosition = {
-                x: this.position.x,
-                y: this.position.y,
-                z: this.position.z
-            };
+            let choppedPosition = { x: this.position.x, y: this.position.y, z: this.position.z };
             if (this.position[this.workingPlane] < this.targetBlock.position[this.workingPlane]) {
                 this.position[this.workingPlane] = this.targetBlock.position[this.workingPlane];
-            }
-            else {
+            } else {
                 choppedPosition[this.workingPlane] += overlap;
             }
             placedMesh.position.set(this.position.x, this.position.y, this.position.z);
             choppedMesh.position.set(choppedPosition.x, choppedPosition.y, choppedPosition.z);
             blocksToReturn.placed = placedMesh;
-            if (!blocksToReturn.bonus)
-                blocksToReturn.chopped = choppedMesh;
-        }
-        else {
+            if (!blocksToReturn.bonus) blocksToReturn.chopped = choppedMesh;
+        } else {
             this.state = this.STATES.MISSED;
         }
         this.dimension[this.workingDimension] = overlap;
@@ -155,22 +139,16 @@ class Block {
     tick() {
         if (this.state == this.STATES.ACTIVE) {
             let value = this.position[this.workingPlane];
-            if (value > this.MOVE_AMOUNT || value < -this.MOVE_AMOUNT)
-                this.reverseDirection();
+            if (value > this.MOVE_AMOUNT || value < -this.MOVE_AMOUNT) this.reverseDirection();
             this.position[this.workingPlane] += this.direction;
             this.mesh.position[this.workingPlane] = this.position[this.workingPlane];
         }
     }
 }
+
 class Game {
     constructor() {
-        this.STATES = {
-            'LOADING': 'loading',
-            'PLAYING': 'playing',
-            'READY': 'ready',
-            'ENDED': 'ended',
-            'RESETTING': 'resetting'
-        };
+        this.STATES = { 'LOADING': 'loading', 'PLAYING': 'playing', 'READY': 'ready', 'ENDED': 'ended', 'RESETTING': 'resetting' };
         this.blocks = [];
         this.state = this.STATES.LOADING;
         this.stage = new Stage();
@@ -189,36 +167,25 @@ class Game {
         this.tick();
         this.updateState(this.STATES.READY);
         document.addEventListener('keydown', e => {
-            if (e.keyCode == 32)
-                this.onAction();
+            if (e.keyCode == 32) this.onAction();
         });
         document.addEventListener('click', e => {
             this.onAction();
         });
         document.addEventListener('touchstart', e => {
             e.preventDefault();
-            // this.onAction();
-            // this triggers after click on android so you
-            // insta-lose, will figure it out later.
         });
     }
     updateState(newState) {
-        for (let key in this.STATES)
-            this.mainContainer.classList.remove(this.STATES[key]);
+        for (let key in this.STATES) this.mainContainer.classList.remove(this.STATES[key]);
         this.mainContainer.classList.add(newState);
         this.state = newState;
     }
     onAction() {
         switch (this.state) {
-            case this.STATES.READY:
-                this.startGame();
-                break;
-            case this.STATES.PLAYING:
-                this.placeBlock();
-                break;
-            case this.STATES.ENDED:
-                this.restartGame();
-                break;
+            case this.STATES.READY: this.startGame(); break;
+            case this.STATES.PLAYING: this.placeBlock(); break;
+            case this.STATES.ENDED: this.restartGame(); break;
         }
     }
     startGame() {
@@ -242,16 +209,13 @@ class Game {
         let countdown = { value: this.blocks.length - 1 };
         TweenLite.to(countdown, cameraMoveSpeed, { value: 0, onUpdate: () => { this.scoreContainer.innerHTML = String(Math.round(countdown.value)); } });
         this.blocks = this.blocks.slice(0, 1);
-        setTimeout(() => {
-            this.startGame();
-        }, cameraMoveSpeed * 1000);
+        setTimeout(() => { this.startGame(); }, cameraMoveSpeed * 1000);
     }
     placeBlock() {
         let currentBlock = this.blocks[this.blocks.length - 1];
         let newBlocks = currentBlock.place();
         this.newBlocks.remove(currentBlock.mesh);
-        if (newBlocks.placed)
-            this.placedBlocks.add(newBlocks.placed);
+        if (newBlocks.placed) this.placedBlocks.add(newBlocks.placed);
         if (newBlocks.chopped) {
             this.choppedBlocks.add(newBlocks.chopped);
             let positionParams = { y: '-=30', ease: Power1.easeIn, onComplete: () => this.choppedBlocks.remove(newBlocks.chopped) };
@@ -264,8 +228,7 @@ class Game {
             };
             if (newBlocks.chopped.position[newBlocks.plane] > newBlocks.placed.position[newBlocks.plane]) {
                 positionParams[newBlocks.plane] = '+=' + (40 * Math.abs(newBlocks.direction));
-            }
-            else {
+            } else {
                 positionParams[newBlocks.plane] = '-=' + (40 * Math.abs(newBlocks.direction));
             }
             TweenLite.to(newBlocks.chopped.position, 1, positionParams);
@@ -283,12 +246,14 @@ class Game {
         this.newBlocks.add(newKidOnTheBlock.mesh);
         this.blocks.push(newKidOnTheBlock);
         this.stage.setCamera(this.blocks.length * 2);
-        if (this.blocks.length >= 5)
-            this.instructions.classList.add('hide');
+        if (this.blocks.length >= 5) this.instructions.classList.add('hide');
     }
     endGame() {
         this.updateState(this.STATES.ENDED);
-         updateHighscore(currentScore);
+
+        // Беремо поточний рахунок і оновлюємо рекорд
+        let currentScore = parseInt(this.scoreContainer.innerText);
+        updateHighscore(currentScore);
     }
     tick() {
         this.blocks[this.blocks.length - 1].tick();
@@ -296,5 +261,7 @@ class Game {
         requestAnimationFrame(() => { this.tick(); });
     }
 }
+
 let game = new Game();
+
         
